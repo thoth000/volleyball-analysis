473b52d0c26657764a9ad4179778a8d2823a0cd6
Sano Yuma <snym719a@gmail.com>
2022-12-07 12:58:06 +0900

    1207

--- program/02_3d_coord/main.py
+++ program/02_3d_coord/main.py
@@ -1,2 +1,18 @@
 import numpy as np
 
+# camera1 data file
+c1_dir          = "../01_camera_position/data_camera1"
+cMatFile_c1     = "{}/camera_mat.csv".format(c1_dir)
+distFile_c1     = "{}/dist.csv".format(c1_dir)
+rotMatFile_c1   = "{}/rotation_mat.csv".format(c1_dir)
+transVecFile_c1 = "{}/trans_vec.csv".format(c1_dir)
+dirVecFile_c1   = "{}/dir_vec.csv".format(c1_dir)
+
+# camera2 data file
+c2_dir          = "../01_camera_position/data_camera2"
+cMatFile_c2     = "{}/camera_mat.csv".format(c2_dir)
+distFile_c2     = "{}/dist.csv".format(c2_dir)
+rotMatFile_c2   = "{}/rotation_mat.csv".format(c2_dir)
+transVecFile_c2 = "{}/trans_vec.csv".format(c2_dir)
+dirVecFile_c2   = "{}/dir_vec.csv".format(c2_dir)
+
