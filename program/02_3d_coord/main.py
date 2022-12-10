7ebda422c474abaadaad560900dc5d3c74654324
Sano Yuma <snym719a@gmail.com>
2022-12-10 18:07:19 +0900

    get 3dCoord

--- program/02_3d_coord/main.py
+++ program/02_3d_coord/main.py
@@ -1,18 +1,74 @@
 import numpy as np
+import cv2
+import csv
+import json
+from coord import *
+
+# camera1 - camera2 ID対応JSON
+correspondFile  = open("correspond.json", "r")
+
+# data
+jsonDataNum = 276
+correspondData  = json.load(correspondFile)
 
 # camera1 data file
-c1_dir          = "../01_camera_position/data_camera1"
-cMatFile_c1     = "{}/camera_mat.csv".format(c1_dir)
-distFile_c1     = "{}/dist.csv".format(c1_dir)
-rotMatFile_c1   = "{}/rotation_mat.csv".format(c1_dir)
-transVecFile_c1 = "{}/trans_vec.csv".format(c1_dir)
-dirVecFile_c1   = "{}/dir_vec.csv".format(c1_dir)
+c1_dir      = "../01_camera_position/data_camera1"
+cMat_c1     = np.loadtxt("{}/camera_mat.csv".format(c1_dir), delimiter=",")
+dist_c1     = np.loadtxt("{}/dist.csv".format(c1_dir), delimiter=",")
+rotMat_c1   = np.loadtxt("{}/rotation_mat.csv".format(c1_dir), delimiter=",")
+transVec_c1 = np.loadtxt("{}/trans_vec.csv".format(c1_dir), delimiter=",")
+dirVec_c1   = np.loadtxt("{}/dir_vec.csv".format(c1_dir), delimiter=",")
 
 # camera2 data file
-c2_dir          = "../01_camera_position/data_camera2"
-cMatFile_c2     = "{}/camera_mat.csv".format(c2_dir)
-distFile_c2     = "{}/dist.csv".format(c2_dir)
-rotMatFile_c2   = "{}/rotation_mat.csv".format(c2_dir)
-transVecFile_c2 = "{}/trans_vec.csv".format(c2_dir)
-dirVecFile_c2   = "{}/dir_vec.csv".format(c2_dir)
+c2_dir      = "../01_camera_position/data_camera2"
+cMat_c2     = np.loadtxt("{}/camera_mat.csv".format(c2_dir), delimiter=",")
+dist_c2     = np.loadtxt("{}/dist.csv".format(c2_dir), delimiter=",")
+rotMat_c2   = np.loadtxt("{}/rotation_mat.csv".format(c2_dir), delimiter=",")
+transVec_c2 = np.loadtxt("{}/trans_vec.csv".format(c2_dir), delimiter=",")
+dirVec_c2   = np.loadtxt("{}/dir_vec.csv".format(c2_dir), delimiter=",")
 
+for i in range(jsonDataNum):
+  jsonName = "{}.json".format(i)
+  
+  c1File = open("c1_json/{}".format(jsonName), "r")
+  c2File = open("c2_json/{}".format(jsonName), "r")
+  
+  c1Data = json.load(c1File)
+  c2Data = json.load(c2File)
+
+  coordData = {}
+  
+  for playerId in c1Data:
+    if playerId in correspondData:
+      correspondId = correspondData[playerId]
+    else:
+      continue
+    
+    if not correspondId in c2Data:
+      continue
+    
+    c1Coord = np.array(c1Data[playerId],     dtype = np.float32)
+    c2Coord = np.array(c2Data[correspondId], dtype = np.float32)
+    
+    #c1UndistortedCoord = cv2.undistortPoints(src = c1Coord, cameraMatrix = cMat_c1, distCoeffs = dist_c1).reshape(-1)
+    #c2UndistortedCoord = cv2.undistortPoints(src = c2Coord, cameraMatrix = cMat_c2, distCoeffs = dist_c2).reshape(-1)
+    
+    #vec_c1 = getHeadingVector(c1UndistortedCoord, cMat_c1, rotMat_c1)
+    #vec_c2 = getHeadingVector(c2UndistortedCoord, cMat_c2, rotMat_c2)
+    
+    vec_c1 = getHeadingVector(c1Coord, cMat_c1, rotMat_c1)
+    vec_c2 = getHeadingVector(c2Coord, cMat_c2, rotMat_c2)
+    
+    worldCoord = compute3dCoord(transVec_c1, vec_c1, transVec_c2, vec_c2)
+    
+    coordData[playerId] = worldCoord.tolist()
+  
+  with open("output/{}".format(jsonName), "w") as f:
+    json.dump(coordData, f)
+  
+  print("write data in {}".format(jsonName))
+  
+  c1File.close()
+  c2File.close()
+
+correspondFile.close()
--- program/utility/convertJSON.py
+++ program/utility/convertJSON.py
@@ -3,15 +3,15 @@ import json
 
 end_index = 275
 
-image_range = ["{}.jpg".format(i) for i in range(0, end_index + 1)]
-
 with open("json_data/c1.json") as c1File:
-    c1Data = c1File
+    c1Data = json.load(c1File)
 
 with open("json_data/c2.json") as c2File:
-    c2Data = c2File
+    c2Data = json.load(c2File)
 
-for imgIndex in image_range:
+for i in range(0, end_index+1):
+    imgIndex = "{}.jpg".format(i)
+    
     c1TmpData = {}
     c2TmpData = {}
     while (True): # c1 roop
@@ -19,11 +19,55 @@ for imgIndex in image_range:
             break
 
         tmpData = c1Data.pop(0)
+        
         if tmpData["image_id"] != imgIndex:
-            tmpData.insert(0, tmpData)
+            c1Data.insert(0, tmpData)
+            break
+        
+        leftHip  = tmpData["keypoints"][11*3 : 11*3+2]
+        rightHip = tmpData["keypoints"][12*3 : 12*3+2]
+
+        if (int(leftHip[0]) == 0) or (int(rightHip[0]) == 0):
+            continue
+        
+        midHip   = [
+            (leftHip[0] + rightHip[0]) / 2,
+            (leftHip[1] + rightHip[1]) / 2
+        ]
+        
+        playerId = int(float(tmpData["idx"]))
+        
+        c1TmpData[playerId] = midHip
+    
+    while (True): # c2 roop
+        if len(c2Data) == 0:
             break
 
-        ExtractedData = {
-            "id" : int(float(tmpData.idx)),
-            "coord" : tmpData.keypoints[]
-        }
+        tmpData = c2Data.pop(0)
+        
+        if tmpData["image_id"] != imgIndex:
+            c2Data.insert(0, tmpData)
+            break
+        
+        leftHip  = tmpData["keypoints"][11*3 : 11*3+2]
+        rightHip = tmpData["keypoints"][12*3 : 12*3+2]
+        
+        if (int(leftHip[0]) == 0) or (int(rightHip[0]) == 0):
+            continue
+        
+        midHip   = [
+            (leftHip[0] + rightHip[0]) / 2,
+            (leftHip[1] + rightHip[1]) / 2
+        ]
+        
+        playerId = int(float(tmpData["idx"]))
+        
+        c2TmpData[playerId] = midHip
+    
+    outputFile = "{}.json".format(i)
+    
+    with open("c1_json/{}".format(outputFile), "w") as f:
+        json.dump(c1TmpData, f)
+        
+    with open("c2_json/{}".format(outputFile), "w") as f:
+        json.dump(c2TmpData, f)
